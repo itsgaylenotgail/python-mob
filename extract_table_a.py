7e9744b3cd46ab566ae64aca492365e9b396704f
gayle <gbmbituin@gmail.com>
2021-02-10 15:17:05 +0800

    add insert operation

--- extract_table_a.py
+++ extract_table_a.py
@@ -9,6 +9,12 @@ class ExtractTableA:
     cntrs = self.lalbdao.get_all_cntrs()
     [print(cntr) for cntr in cntrs]
 
+    data = ("2115840602","CSLU600902","PCC1-OSN-100N", "OOCL SOUTHAMPTON", "LGB10",
+            "Long Beach Container Terminal (Pier E)", "America/Los_Angeles","533SCJG", "1")
+    new_cntr = self.lalbdao.insert_new_cntr(data)
+
+    print("NEW CONTAINER ADDED WITH ID: " + str(new_cntr))
+
 
 if __name__ == '__main__':
     ExtractTableA().main()
--- lalb_dao.py
+++ lalb_dao.py
@@ -9,6 +9,17 @@ class LALBDAO:
     cntrs = self.mysql.get_db_cursor().fetchall()
     return cntrs
 
+  def insert_new_cntr(self, fields):
+    self.mysql.get_db_cursor().execute(LALBDAO.insert_all_cnts_details_sql(), fields)
+    cntr = self.mysql.get_db_cursor().lastrowid
+    self.mysql.get_db_connection().commit()
+    return cntr
+
   @staticmethod
   def get_all_cnts_details_sql():
     return "SELECT * FROM CNTR_DETAILS"
+
+  @staticmethod
+  def insert_all_cnts_details_sql():
+    return "INSERT INTO CNTR_DETAILS (BL_NUM, CNTR_NUM, SVVD, VESSEL_NAME, FACILITY_CDE, PICKUP_LOC, " \
+           "PICKUP_LOC_TZ, MVMT_SPEC, CHECK_DIGIT ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
